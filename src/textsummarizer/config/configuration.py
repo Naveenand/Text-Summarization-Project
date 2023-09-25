78c1e918a4ad3dee0e241a67205c02c87ee638f2
naveenand <naveenyadav135100@gmail.comgit config --global user.email naveenyadav135100@gmail.comgit config --global user.email naveenyadav135100@gmail.com>
2023-09-25 23:25:51 +0530

    hi middle

--- src/textsummarizer/config/configuration.py
+++ src/textsummarizer/config/configuration.py
@@ -2,7 +2,7 @@ from textsummarizer import *
 from textsummarizer.utils.common import read_yaml, create_directories
 from textsummarizer.constants import *
 from textsummarizer.entity import *
-
+from textsummarizer.entity import DataTransformationConfig
 
 
 
@@ -48,3 +48,17 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name = config.tokenizer_name
+        )
+
+        return data_transformation_config
+    
